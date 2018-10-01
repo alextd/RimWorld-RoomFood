6faad337f5e9fd5b9950c38e7b52b21894ec9ca4
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2018-09-30 18:58:33 -0700

    suppress DefOf warning on load

--- Source/Room_Food.cs
+++ Source/Room_Food.cs
@@ -2,6 +2,7 @@
 using Verse;
 using UnityEngine;
 using Harmony;
+using RimWorld;
 
 namespace Room_Food
 {
@@ -15,7 +16,18 @@ namespace Room_Food
 			HarmonyInstance.DEBUG = true;
 #endif
 			HarmonyInstance harmony = HarmonyInstance.Create("Uuugggg.rimworld.Room_Food.main");
+			
+			//Turn off DefOf warning since harmony patches trigger it.
+			harmony.Patch(AccessTools.Method(typeof(DefOfHelper), "EnsureInitializedInCtor"),
+				new HarmonyMethod(typeof(Mod), "EnsureInitializedInCtorPrefix"), null);
+			
 			harmony.PatchAll();
+		}
+
+		public static bool EnsureInitializedInCtorPrefix()
+		{
+			//No need to display this warning.
+			return false;
 		}
 
 		// public override void DoSettingsWindowContents(Rect inRect)
